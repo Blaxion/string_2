d4a1363b67299d5d833ff02f9cedb3afa5f48919
Blaxion <khibaziev.f@gmail.com>
2021-04-16 00:39:26 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// 1
+let phrase = "Bonjour tout le monde   ";
+
+// 2
+console.log(phrase.length)
+
+// 3
+phrase=phrase.substring(0,21)
+
+// 4
+console.log(phrase.length);
+
+// 5
+console.log( phrase.substring(20,21));
+
+// 6
+halfphrase=phrase.substring(0,8) 
+phrase=phrase.substring(8,21)
+
+// 7
+phrase=halfphrase+phrase 
+
+// 8
+halfphrase=phrase.substring(0,8) 
+
+// 9
+phrase.replace('number','phrase')
+
+// 10
+Math.random()
+
+
